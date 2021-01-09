5461a3e6a67478590622d9e45762e075cce8da26
Mukul-dhiman <orton1615@gmail.com>
2021-01-09 17:40:17 +0530

    sidebar animation

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,23 +1,11 @@
-$(document).ready(function () {
-    $(".ion-ios-navicon").on("touchstart click", function(e) {
-        console.log("click")
-        e.preventDefault(), 
-        $(".sidebar").toggleClass("active"), 
-        $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated")
-    }),
+$(document).ready(function() {
+    $(".ion-ios-navicon").click(function () {
+
+            $(".sidebar").toggleClass("active"),
+                $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated");
+        });
     $(".sidebar .sidebar-overlay").on("touchstart click", function(e) {
         e.preventDefault(), 
         $(".ion-ios-navicon").click()
-    })
-    
+    });
 });
-$(".ion-ios-navicon").on("touchstart click", function(e) {
-    console.log("click")
-    e.preventDefault(), 
-    $(".sidebar").toggleClass("active"), 
-    $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated")
-}),
-$(".sidebar .sidebar-overlay").on("touchstart click", function(e) {
-    e.preventDefault(), 
-    $(".ion-ios-navicon").click()
-})
