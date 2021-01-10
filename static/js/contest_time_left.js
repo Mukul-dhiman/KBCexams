bfeded3e922d5e5b8f9690271094698bc74ffb11
Mukul-dhiman <orton1615@gmail.com>
2021-01-11 01:06:33 +0530

    contest end timer

---
+++ static/js/contest_time_left.js
@@ -0,0 +1,26 @@
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 36));
+    return days + ":" + hours + ":" + minutes + ":" + seconds ;
+  
+  }
+  
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+  
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+
+      clock.innerHTML = t;
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
